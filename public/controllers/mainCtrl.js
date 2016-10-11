23445249340123358f011ec231969e7d1663e354
Theo Beck <eduinfo96@yahoo.com>
2016-10-11 14:41:17 -0500

    first commit

---
+++ public/controllers/mainCtrl.js
@@ -0,0 +1,22 @@
+angular.module("movieMe").controller("mainCtrl", function($scope, mainServ) {
+    // $scope.register = "Deez Noodles";
+    //
+    // $scope.getUserLocation = function() {
+    //     mainServ.getUserLocation().then(function(result) {
+    //         $scope.userLocation = result;
+    //     })
+    // }
+    //
+    // $scope.getUserLocation();
+
+
+  //   // Find User's location
+  //   $scope.getLoc = navigator.geolocation.getCurrentPosition(function success(position) {
+  //   var latitude  = position.coords.latitude;
+  //   var longitude = position.coords.longitude;
+  //
+  //   console.log('<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>');
+  // });
+
+
+});
---
+++ public/services/mainServ.js
@@ -0,0 +1,31 @@
+angular.module("movieMe").service("mainServ", function($http) {
+    // const distUrl = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='+ user1Lat + ',' + user1Lon + '&destinations=' + user2Lat + ',' + user2Lon + '&key=' + distKey;
+    // const distKey = 'AIzaSyBQG4gzjr4KzXIpJlJc6WYoMlWqG7X7J3I';
+    //const onConnBase = 'http://data.tmsapi.com/v1.1/movies/showings?startDate=' + newDate + '&lat=' + userLat + '&lng=' + userLong + '&radius=15&units=mi&api_key='
+    // **Format New Date**
+    // const onConnKey = '28vjfpqe5kzaekmb34q2d4xf';
+    //
+    //
+    // this.getUserLocation = function() {
+    //     return $http.post(geoUrl + geoKey, {}).then(function(response) {
+    //         if (response.status = 200) {
+    //             console.log(response.data);
+    //             return response.data;
+    //         } else {
+    //             console.log("There was an Error!")
+    //         };
+    //     })
+    // }
+
+    /*
+
+    Match Users:
+      1. Pull user objects into an array based off theater selected
+      2. Loop through array looking for a match based of search criteria
+      3. Have a "isMatched" boolean property on each user
+
+      */
+
+
+
+})
