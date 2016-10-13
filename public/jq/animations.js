636d838380bdf65c83d02fae65df8f332de0c24c
Theo Beck <eduinfo96@yahoo.com>
2016-10-13 00:32:33 -0500

    Navbar

---
+++ public/jq/animations.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if( $(document).scrollTop() > 100) {
+      $(".title-nav").addClass("moved-title");
+      $(".link-nav").addClass("moved-link");
+      $("nav ul li").addClass("moved-links");
+      $(".title").addClass("moved-title-2");
+    }
+    else{
+      $(".title-nav").removeClass("moved-title");
+      $(".link-nav").removeClass("moved-link");
+      $("nav ul li").removeClass("moved-links");
+      $(".title").removeClass("moved-title-2");
+    }
+  })
+
+
+
+
+
+
+});
